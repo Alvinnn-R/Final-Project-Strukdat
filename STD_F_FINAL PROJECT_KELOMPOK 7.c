ba8aae7344054d7ac0a2bc468dc8c27a5ae90346
Alvin Rama Saputra <108909805+Alvinnn-R@users.noreply.github.com>
2024-12-15 18:47:39 +0700

    Update STD_F_FINAL PROJECT_KELOMPOK 7.c

--- STD_F_FINAL PROJECT_KELOMPOK 7.c
+++ STD_F_FINAL PROJECT_KELOMPOK 7.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/* Penerapan Sistem Absensi Sekolah FP Struktur Data
+1. Linked List: menyimpan daftar absen
+2. Queue: memastikan siswa, guru dan staff diabsen berdasarkan urutan (FIFO)
+3. Tree : untuk membuat percabangan berdasarkan jenis pengguna, apakah seorang guru, siswa, atau staff*/
+
 int main() {
 	
 	printf("Hello World");
